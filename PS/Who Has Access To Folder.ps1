c5e55df7a2bb4b67cf695e99e5c8c2c8276e154d
AyzinA <103305265+AyzinA@users.noreply.github.com>
2022-04-09 20:34:00 +0300

    Update Who Has Access To Folder.ps1

--- PS/Who Has Access To Folder.ps1
+++ PS/Who Has Access To Folder.ps1
@@ -5,7 +5,7 @@ $mainEnd = ".com"
 
 $userList = (get-acl -Path $path).Access | Select IdentityReference
 foreach($user in $userList){
-if($user.IdentityReference.ToString().Split("\")[0] -eq "clalit"){$D = $user.IdentityReference.ToString().Split("\")[0] + $mainEnd}
+if($user.IdentityReference.ToString().Split("\")[0] -eq $mainDomain){$D = $user.IdentityReference.ToString().Split("\")[0] + $mainEnd}
 else{$D = $user.IdentityReference.ToString().Split("\")[0] + "." + $mainDomain + $mainEnd }
 try{$U = (Get-ADUser -Identity $user.IdentityReference.ToString().Split("\")[1] -Server $D)
 if($U){$U.Name}}
