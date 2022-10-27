467bb26bcbb826bb44b4d3d71119c052c72b48a8
AyzinA <103305265+AyzinA@users.noreply.github.com>
2022-10-27 05:52:12 +0300

    Create Sound.ps1

---
+++ Sound.ps1
@@ -0,0 +1,10 @@
+#From URL
+
+$MediaPlayer = [Windows.Media.Playback.MediaPlayer, Windows.Media, ContentType = WindowsRuntime]::New()
+$MediaPlayer.Source = [Windows.Media.Core.MediaSource]::CreateFromUri('https://nyanpass.com/nyanpass.mp3')
+$MediaPlayer.Play()
+#From File
+
+$MediaPlayer = [Windows.Media.Playback.MediaPlayer, Windows.Media, ContentType = WindowsRuntime]::New()
+$MediaPlayer.Source = [Windows.Media.Core.MediaSource]::CreateFromUri('C:\Users\Admin\Downloads\nyanpass.mp3')
+$MediaPlayer.Play()
