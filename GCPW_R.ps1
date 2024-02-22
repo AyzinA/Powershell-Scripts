4f1eb0cb63522e202a8a288fa1ca9ac512d759a4
AyzinA <103305265+AyzinA@users.noreply.github.com>
2024-02-22 14:21:01 +0200

    Update GCPW_R.ps1

--- GCPW_R.ps1
+++ GCPW_R.ps1
@@ -44,17 +44,16 @@ Invoke-WebRequest -Uri $gcpwUri -OutFile $gcpwFileName
 
 <# Run the GCPW installer and wait for the installation to finish #>
 $arguments = "/i `"$pwd\$gcpwFileName`""
-$pwd/$gcpwFileName
 $installProcess = (Start-Process msiexec.exe -ArgumentList $arguments -PassThru -Wait)
 
 <# Check if installation was successful #>
 if ($installProcess.ExitCode -ne 0) {
     $result = [System.Windows.MessageBox]::Show('Installation failed!', 'GCPW', 'OK', 'Error')
-    Remove-Item -Path .\$gcpwFileName -Recurse -Force
+    Remove-Item -Path $gcpwFileName -Recurse -Force
     exit $installProcess.ExitCode
 }
 else {
-    Remove-Item -Path .\$gcpwFileName -Recurse -Force
+    Remove-Item -Path $gcpwFileName -Recurse -Force
     $result = [System.Windows.MessageBox]::Show('Installation completed successfully!', 'GCPW', 'OK', 'Info')
 }
 
