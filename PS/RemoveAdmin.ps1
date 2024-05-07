6c3d560a97e516e4a77cf23927ca4f6424154830
AyzinA <103305265+AyzinA@users.noreply.github.com>
2024-05-07 13:29:44 +0300

    fix

--- PS/RemoveAdmin.ps1
+++ PS/RemoveAdmin.ps1
@@ -1,14 +1,14 @@
 $members = ((Get-LocalGroupMember -Group Administrators).Name | Where-Object { $_ -notlike 'NT AUTHORITY*' } | ForEach-Object { $_.Split('\')[1] })
 
-$MembersToCheckAndRemoveAdmin = @()
+$membersWithAdmin = @()
 foreach ($member in $members)
 {
 	if ($member -ne "Administrator" -And $member -ne "admin")
 	{
- 		$MembersToCheckAndRemoveAdmin += $member
+ 		$membersWithAdmin += $member
  	}
 }
-foreach ($user in $MembersToCheckAndRemoveAdmin )
+foreach ($user in $membersWithAdmin )
 {
 	Remove-LocalGroupMember -Group Administrators -Member $user
 }
